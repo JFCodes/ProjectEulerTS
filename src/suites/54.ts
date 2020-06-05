bbd77aec5f76b2b15f23febdba5f53dd73b599fe
Joaquim Fonseca <joaquim.fonseca@mercedes-benz.io>
2020-06-05 16:12:03 +0100

    Merge Hand type with CardMatrix class

--- src/suites/54.ts
+++ src/suites/54.ts
@@ -8,11 +8,6 @@ enum SUITES {
     HEARTS = 'H'
 }
 
-type HAND = {
-    cardMatrix: CARD_MATRIX,
-    classification: number | null
-}
-
 enum HAND_VALUES {
     HIGH_CARD = 0,
     ONE_PAIR = 1,
@@ -41,8 +36,8 @@ class suite implements Suite {
 
         POKER_PLAYS.forEach((pokerPlay) => {
             const cards = pokerPlay.split(' ')
-            const hand1 = this.stringCardsToHand([cards[0],  cards[1], cards[2], cards[3], cards[4] ])
-            const hand2 = this.stringCardsToHand([cards[5],  cards[6], cards[7], cards[8], cards[9] ])
+            const hand1 = new HAND([cards[0],  cards[1], cards[2], cards[3], cards[4] ])
+            const hand2 = new HAND([cards[5],  cards[6], cards[7], cards[8], cards[9] ])
 
             if (this.p1Wins(hand1, hand2)) result++
         })
@@ -60,10 +55,10 @@ class suite implements Suite {
     }
 
     private p1WinsTieBreak(hand1: HAND, hand2: HAND): boolean | null {
-        const extractOcurranceIndex = (hand: HAND, occur: number): number[] => {
+        const extractOcurranceIndexes = (hand: HAND, occur: number): number[] => {
             const values = []
             let hasAce = false
-            hand.cardMatrix.numbers.forEach((count, index) => {
+            hand.matrix.numbers.forEach((count, index) => {
                 if (count !== occur) return
                 if (index === 0) {
                     hasAce = true
@@ -84,16 +79,16 @@ class suite implements Suite {
         // straight special case
         // for the straigh case a A-5 loses to all other straights
         // If hands have a straight
-        if (hand1.cardMatrix.checks.hasStraigth) {
+        if (hand1.checks.hasStraigth) {
             // If hand 1 is straight A -> 5 and hand 2 does not
-            if (hand1.cardMatrix.checks.hasLowAceStraight && !hand2.cardMatrix.checks.hasLowAceStraight) {
+            if (hand1.checks.hasLowAceStraight && !hand2.checks.hasLowAceStraight) {
                 return false
             }
         }
 
         for (let occurance = 4; occurance > 0; occurance--) {
-            p1Values = extractOcurranceIndex(hand1, occurance)
-            p2Values = extractOcurranceIndex(hand2, occurance)
+            p1Values = extractOcurranceIndexes(hand1, occurance)
+            p2Values = extractOcurranceIndexes(hand2, occurance)
             for (let index = 0; index < p1Values.length; index++) {
                 if (p1Values[index] > p2Values[index]) return true
                 if (p1Values[index] < p2Values[index]) return false
@@ -102,23 +97,16 @@ class suite implements Suite {
 
         return null
     }
-
-    private stringCardsToHand(cards: string[]): HAND {
-        const hand: HAND = {
-            cardMatrix: new CARD_MATRIX(cards),
-            classification: 0
-        }
-        hand.classification = hand.cardMatrix.getClassification()
-        return hand
-    }
 }
 
 // Card matrix class
-class CARD_MATRIX {
-    numbers: number[]
-    suites: { [index: string]: number }
-    // A hand classification is combination of the following checks
-    public checks: {
+class HAND {
+    cards: string[]
+    matrix: {
+        numbers: number[]
+        suites: { [index: string]: number }
+    }
+    checks: {
         hasOnePair: boolean
         hasTwoPair: boolean
         hasTriple: boolean
@@ -127,56 +115,61 @@ class CARD_MATRIX {
         hasLowAceStraight: boolean
         hasFlush: boolean
     }
+    classification: number
 
     constructor(cards: string[]) {
-        this.numbers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        this.suites = {
-            [SUITES.CLUBS]: 0,
-            [SUITES.DIAMONDS]: 0,
-            [SUITES.SPADES]: 0,
-            [SUITES.HEARTS]: 0,
+        this.cards = cards
+        this.matrix = {
+            numbers: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            suites: {
+                [SUITES.CLUBS]: 0,
+                [SUITES.DIAMONDS]: 0,
+                [SUITES.SPADES]: 0,
+                [SUITES.HEARTS]: 0,
+            }
         }
         
         cards.forEach((card) => {
-            this.suites[card[1]]++
+            this.matrix.suites[card[1]]++
 
             switch (card[0]) {
                 case 'A':
-                    this.numbers[0]++
+                    this.matrix.numbers[0]++
                     break
                 case 'T':
-                    this.numbers[9]++
+                    this.matrix.numbers[9]++
                     break
                 case 'J':
-                    this.numbers[10]++
+                    this.matrix.numbers[10]++
                     break
                 case 'Q':
-                    this.numbers[11]++
+                    this.matrix.numbers[11]++
                     break
                 case 'K':
-                    this.numbers[12]++
+                    this.matrix.numbers[12]++
                     break
                 default:
-                    this.numbers[Number(card[0]) - 1]++
+                    this.matrix.numbers[Number(card[0]) - 1]++
                     break
             }
         })
 
         const hasStraight = this.getHasStraigth()
         this.checks = {
-            hasOnePair: this.numbers.filter(count => count == 2).length === 1,
-            hasTwoPair: this.numbers.filter(count => count == 2).length === 2,
-            hasTriple: this.numbers.filter(count => count == 3).length === 1,
-            hasFour: this.numbers.filter(count => count === 4).length === 1,
+            hasOnePair: this.matrix.numbers.filter(count => count == 2).length === 1,
+            hasTwoPair: this.matrix.numbers.filter(count => count == 2).length === 2,
+            hasTriple: this.matrix.numbers.filter(count => count == 3).length === 1,
+            hasFour: this.matrix.numbers.filter(count => count === 4).length === 1,
             hasStraigth: hasStraight,
             hasLowAceStraight: hasStraight ? this.getHasLowAceStraight() : false,
-            hasFlush: Object.values(this.suites).some(count => count === 5)
+            hasFlush: Object.values(this.matrix.suites).some(count => count === 5)
         }
+        this.classification = this.getClassification()
     }
 
-    public getClassification(): HAND_VALUES {
+    private getClassification(): HAND_VALUES {
         if (this.checks.hasFlush && this.checks.hasStraigth) {
-            if (this.numbers[0] === 1 && this.numbers[9] === 1) return HAND_VALUES.ROYAL_FLUSH
+            if (this.matrix.numbers[0] === 1 && this.matrix.numbers[9] === 1) return HAND_VALUES.ROYAL_FLUSH
             return HAND_VALUES.STRAIGHT_FLUSH
         }
         if (this.checks.hasFour) return HAND_VALUES.FOUR
@@ -192,23 +185,23 @@ class CARD_MATRIX {
 
     private getHasStraigth(): boolean {
         // All counts must be 0 or 1 to have a straigth
-        if (this.numbers.filter(count => count > 1).length > 0) return false
+        if (this.matrix.numbers.filter(count => count > 1).length > 0) return false
 
-        for (let i = 0; i < this.numbers.length - 3; i++) {
-            if (this.numbers[i] !== 1) continue
-            if (this.numbers[i] !== this.numbers[i  + 1]) continue
-            if (this.numbers[i] !== this.numbers[i  + 2]) continue
-            if (this.numbers[i] !== this.numbers[i  + 3]) continue
-            if (this.numbers[i] !== this.numbers[i  + 4]) continue
+        for (let i = 0; i < this.matrix.numbers.length - 3; i++) {
+            if (this.matrix.numbers[i] !== 1) continue
+            if (this.matrix.numbers[i] !== this.matrix.numbers[i  + 1]) continue
+            if (this.matrix.numbers[i] !== this.matrix.numbers[i  + 2]) continue
+            if (this.matrix.numbers[i] !== this.matrix.numbers[i  + 3]) continue
+            if (this.matrix.numbers[i] !== this.matrix.numbers[i  + 4]) continue
             return true
         }
 
         // Ace over king special case
-        if (this.numbers[9] === 1) {
-            if (this.numbers[10] !== 1) return false
-            if (this.numbers[11] !== 1) return false
-            if (this.numbers[12] !== 1) return false
-            if (this.numbers[0] !== 1) return false
+        if (this.matrix.numbers[9] === 1) {
+            if (this.matrix.numbers[10] !== 1) return false
+            if (this.matrix.numbers[11] !== 1) return false
+            if (this.matrix.numbers[12] !== 1) return false
+            if (this.matrix.numbers[0] !== 1) return false
             return true
         }
 
@@ -218,7 +211,7 @@ class CARD_MATRIX {
     private getHasLowAceStraight(): boolean {
         // Only runs if already checked for straight
         // Is low ace straght if index 0 and 1 counts are 1
-        return this.numbers[0] === 1 && this.numbers[1] === 1
+        return this.matrix.numbers[0] === 1 && this.matrix.numbers[1] === 1
     }
 }
 
